1c4a41a9efc584261797c2c677d90921ddc46880
gurusaran007 <55083014+gurusaran007@users.noreply.github.com>
2024-01-11 21:44:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+var value = document.createElement('h1');
+value.className="main";
+document.body.append(value);
+
+var num=10;
+setTimeout(()=>{
+    document.querySelector('h1').innerText= `I'm Counting: ${num=num-1}`;
+    
+    setTimeout(()=>{
+        
+        document.querySelector('h1').innerText= `I'm Counting: ${num=num-1}`;
+        
+        setTimeout(()=>{
+            document.querySelector('h1').innerText= `I'm Counting: ${num=num-1}`;
+            
+            setTimeout(()=>{
+
+                document.querySelector('h1').innerText= `I'm Counting: ${num=num-1}`;
+               
+                setTimeout(()=>{
+
+                    document.querySelector('h1').innerText= `I'm Counting: ${num=num-1}`;
+
+                    setTimeout(()=>{
+
+                        document.querySelector('h1').innerText= `I'm Counting: ${num=num-1}`;
+
+                        setTimeout(() =>{
+
+                            document.querySelector('h1').innerText= `I'm Counting: ${num=num-1}`;
+                            setTimeout(() =>{
+
+                                document.querySelector('h1').innerText= `I'm Counting: ${num=num-1}`;
+                                setTimeout(() =>{
+
+                                    document.querySelector('h1').innerText= `I'm Counting: ${num=num-1}`;
+
+                                    setTimeout(()=>{
+
+                                        document.querySelector('h1').innerText= `Happy Independence Day`;
+
+ 
+                                     
+                                    },1000)
+
+                                },1000)
+                            },1000)
+                        },1000)
+                    },1000)
+                    
+                },1000)
+            },1000)
+        },1000)
+    },1000)
+})
+
+
+    
